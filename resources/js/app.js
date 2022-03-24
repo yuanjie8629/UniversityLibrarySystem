0f12a0b78f05999d0754ae993aa0a33941c84dba
IQ9999999 <52337440+IQ9999999@users.noreply.github.com>
2022-03-24 21:13:25 +0800

    setting vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,10 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue').default;
+window.Vue = require("vue").default;
+import vuetify from "./vuetify";
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,7 +20,10 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -28,5 +32,6 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  */
 
 const app = new Vue({
-    el: '#app',
+    el: "#app",
+    vuetify,
 });
